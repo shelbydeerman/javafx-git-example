0a8d39cace59ec280e08c59c27b6cf64e7e414dc
shelbydeerman <shelby.r.deerman@saic.com>
2021-06-21 13:49:12 -0500

    Initial commit

---
+++ src/sample/Main.java
@@ -0,0 +1,19 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
