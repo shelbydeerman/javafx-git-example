cbb54cdcb87716e03c731eb5c509eadc66283591
shelbydeerman <shelby.r.deerman@saic.com>
2021-06-21 14:59:03 -0500

    Add drop-down selection and button

--- src/sample/Purple.java
+++ src/sample/Purple.java
@@ -1,14 +1,31 @@
 package sample;
 
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.layout.HBox;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class Purple {
 
     public void setup(Stage stage) {
-        HBox root = new HBox();
+
+        ComboBox<String> colorChoice = new ComboBox<>();
+        colorChoice.getItems().add("red");
+        colorChoice.getItems().add("blue");
+
+        Button select = new Button("Select");
+
+        VBox root = new VBox(15);
+        root.getChildren().addAll(colorChoice, select);
+        root.setAlignment(Pos.CENTER);
+        root.setStyle("-fx-background-color: transparent");
         Scene scene = new Scene(root);
         scene.setFill(Color.MEDIUMPURPLE);
         stage.setScene(scene);
@@ -16,5 +33,4 @@ public class Purple {
         stage.setHeight(250);
         stage.show();
     }
-
 }
